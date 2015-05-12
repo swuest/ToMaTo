67364654d3eade6468e6e78f68ec021053b180ee
swuest <swuest@cs.uni-kl.de>
2015-05-12 16:47:08 +0200

    bugfix

--- backend/tomato/host.py
+++ backend/tomato/host.py
@@ -1055,8 +1055,8 @@ def reallocate():
 	#needs to be redefined at a better place
 	THRESHOLD = 20
 	#Walk through all elements and think about reallocating them.
-	print(elements.getAll({}))
-	for el in elements.getAll({}):
+	print(elements.getAll())
+	for el in elements.getAll():
 		if el.state in ["started","created"]:
 			continue
 		hostPref, sitePref = el.getLocationPrefs()
