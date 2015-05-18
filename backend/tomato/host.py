8bab4dd6e4432e696e840c4252cd8b9309044253
swuest <swuest@cs.uni-kl.de>
2015-05-18 10:44:44 +0200

    Bugfix

--- backend/tomato/host.py
+++ backend/tomato/host.py
@@ -1052,12 +1052,12 @@ def getBestHost(site=None, elementTypes=None, connectionTypes=None,networkKinds=
 	
 def reallocate():
 	
-	from elements import *
+	import elements
 	#needs to be redefined at a better place
 	THRESHOLD = 20
 	#Walk through all elements and think about reallocating them.
 	
-	for el in Element.getAll():
+	for el in elements.getAll():
 		if el.state in ["started","created"]:
 			continue
 		hostPref, sitePref = el.getLocationPrefs()
